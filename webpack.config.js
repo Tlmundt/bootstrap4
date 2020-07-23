5381d8a72331b04dadafee244b69574fa35feb7b
Trevor Mundt <40044625+trevormundt@users.noreply.github.com>
2020-07-23 13:01:46 -0700

    Fix webpack.config to allow HMR & Live Reload on all HTML pages
    Fixed this by adding chunks to all the HtmlWebpackPlugins that
    generated all the html pages, allowing them to use the same app
    entry point in webpack.
    
    Also removed devDependencies that are no longer necessary at
    this build stage.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js'
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     },
     devServer: {
 		watchContentBase: true,
+		watchOptions: {
+            ignored: /node_modules/
+        },
 	    compress: true,
 	    stats: {
 		    all: false,
@@ -19,6 +24,7 @@ module.exports = {
 		    warnings: true,
 		    errorDetails: true
 	    },
+	    inline: true,
 	    hot: true,
 	    host: '0.0.0.0',
 	    port: 8080,
@@ -27,139 +33,157 @@ module.exports = {
 	},
 	module: {
     	rules: [
-        	{
-            	test: /\.html$/,
-            	use: [
-                	{
-                    	loader: 'file-loader',
-                    	options: {
-                        	name: '[name].[ext]'
-                    	}
-                	}
-            	],
-            	exclude: path.resolve(__dirname, 'src/index.html')
-        	}
+        	
     	]
 	},
 	optimization: {
 	    minimize: false
 	},
 	plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new HtmlWebpackPlugin({
-                filename: 'index.html',
-                template: 'src/index.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_2_basic_typography.html',
-            })
-            ,
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_3_text_alignment_display.html',
-            })
-            ,
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_4_floats_position.html',
-            })
-            ,
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_5_colors_background.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_6_spacing.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_7_sizing.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/2_8_breakpoints.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_2_buttons.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_3_navbar.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_4_list_groups_badges.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_5_forms.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_6_input_groups.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_7_alerts_progress.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_8_tables_pagination.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_9_cards.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_10_media_object.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/3_11_jumbotron.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/4_2_grid_system.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/4_3_grid_alignment.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/4_4_flexbox.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/4_5_auto_margins_wrapping_order.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_2_carousel.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_3_collapse.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_4_tooltips.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_5_popovers.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_6_modals.html',
-            }),
-            new HtmlWebpackPlugin({
-                filename: '[name].[ext]',
-                template: 'src/html-pages/5_7_scrollspy.html',
-            })
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '2_2_basic_typography.html',
+            template: 'src/html-pages/2_2_basic_typography.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '2_3_text_alignment_display.html',
+            template: 'src/html-pages/2_3_text_alignment_display.html',
+            chunks: ['app']
+        })
+        ,
+        new HtmlWebpackPlugin({
+            filename: '2_4_floats_position.html',
+            template: 'src/html-pages/2_4_floats_position.html',
+            chunks: ['app']
+        })
+        ,
+        new HtmlWebpackPlugin({
+            filename: '2_5_colors_background.html',
+            template: 'src/html-pages/2_5_colors_background.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '2_6_spacing.html',
+            template: 'src/html-pages/2_6_spacing.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '2_7_sizing.html',
+            template: 'src/html-pages/2_7_sizing.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '2_8_breakpoints.html',
+            template: 'src/html-pages/2_8_breakpoints.html',
+            chunks: ['app']
+        }),
+/*
+        new HtmlWebpackPlugin({
+            filename: '3_2_buttons.html',
+            template: 'src/html-pages/3_2_buttons.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_3_navbar.html',
+            template: 'src/html-pages/3_3_navbar.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_4_list_groups_badges.html',
+            template: 'src/html-pages/3_4_list_groups_badges.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_5_forms.html',
+            template: 'src/html-pages/3_5_forms.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_6_input_groups.html',
+            template: 'src/html-pages/3_6_input_groups.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_7_alerts_progress.html',
+            template: 'src/html-pages/3_7_alerts_progress.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_8_tables_pagination.html',
+            template: 'src/html-pages/3_8_tables_pagination.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_9_cards.html',
+            template: 'src/html-pages/3_9_cards.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_10_media_object.html',
+            template: 'src/html-pages/3_10_media_object.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '3_11_jumbotron.html',
+            template: 'src/html-pages/3_11_jumbotron.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '4_2_grid_system.html',
+            template: 'src/html-pages/4_2_grid_system.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '4_3_grid_alignment.html',
+            template: 'src/html-pages/4_3_grid_alignment.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '4_4_flexbox.html',
+            template: 'src/html-pages/4_4_flexbox.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '4_5_auto_margins_wrapping_order.html',
+            template: 'src/html-pages/4_5_auto_margins_wrapping_order.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_2_carousel.html',
+            template: 'src/html-pages/5_2_carousel.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_3_collapse.html',
+            template: 'src/html-pages/5_3_collapse.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_4_tooltips.html',
+            template: 'src/html-pages/5_4_tooltips.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_5_popovers.html',
+            template: 'src/html-pages/5_5_popovers.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_6_modals.html',
+            template: 'src/html-pages/5_6_modals.html',
+            chunks: ['app']
+        }),
+        new HtmlWebpackPlugin({
+            filename: '5_7_scrollspy.html',
+            template: 'src/html-pages/5_7_scrollspy.html',
+            chunks: ['app']
+        })
+*/
+        new webpack.HotModuleReplacementPlugin()
 	]
 };
